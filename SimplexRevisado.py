a1b35bb20805ae45e0bd3858fe94598833ff9ac8
MarconyH <marconyhenrique321@gmail.com>
2025-01-26 22:51:17 -0300

    Funciona, menos o FO Co-authored-by: Marcony <MarconyH@users.noreply.github.com>

--- SimplexRevisado.py
+++ SimplexRevisado.py
@@ -2,12 +2,12 @@ import numpy as np
 import sys
 global Quan_R ## quantidade de linhas de restrições
 global QuantV  ##quantidade de variaveis
-global C
-
+global C, Vartificiais
+global Solution
 Quan_R = 0
 QuantV = 0
-Vartificiais = []#guardar variaveis artificial
-
+Vartificiais = 0 #guardar variaveis artificial
+global Indice, col_troca
 #Quan_R = 3
 #QuantV = 2
  ##haro heheheheh
@@ -22,7 +22,7 @@ R = []
 B = []
 
 def BuscaInt(a_lin):
-    integers = [int(num) for num in a_lin.split() if num.lstrip('-').isdigit()]  ##split == divide strings em caracteres
+    integers = [float(num) for num in a_lin.split() if num.lstrip('-').isdigit()]  ##split == divide strings em caracteres
     return integers
 
 def MaxOrMin(line):
@@ -56,10 +56,18 @@ def VarExcFol(line, linha_c):
             A_new[linha_c][QuantV] = -1
             QuantV += 1
             A_new[linha_c][QuantV] = 1
-            A_new[0][QuantV] = 1
+            QuantV += 1
+            Vartificiais += 1
             Matriz = np.array(A_new)
             C = np.append(C, [0])
-            C = np.append(C, [1])
+        elif( j == '='):
+            newcol = np.zeros(Quan_R)
+            new_M = np.insert(Matriz, QuantV, newcol, axis = 1)
+            new_M[linha_c][QuantV] = 1
+            QuantV +=1
+            Vartificiais += 1
+            Matriz = np.array(new_M) 
+    C = np.append(C, [1])
 
 def LerArquivo():
     global Quan_R, QuantV
@@ -109,52 +117,87 @@ R = ProcurarColuna(cr)
 B = ProcurarColuna(cb)
 
 def trocar_colunas(col1, col2, matriz = None):
+    global cb, cr
     if(matriz is None):
         Matriz[:, [col1, col2]] = Matriz[:, [col2, col1]]
     elif(matriz.ndim == 1):  ##verifica se é apenas um vetor
-        matriz[[col1, col2]] = matriz[[col2, col1]] 
+        matriz[[col1, col2]] = matriz[[col2, col1]]
 
 def EntrarBase():
+    global Matriz, R, B, C, cr, cb, R, B, b, Solution, col_troca, Indice, new_C
     Indice = ProcurarColuna(cr, C)
-    Indice = np.argmin(Indice)
+    Indice = np.argmin(Indice) #Cr
     if(C[Indice] < 0):
         ColEntrada = Matriz[:, Indice]
         ColEntrada = ColEntrada.reshape(-1, 1)
+        
         LinSaida = np.argmin(np.divide(b, ColEntrada))
-        col_troca = np.where(Matriz[LinSaida, :] == 1)[0]  ##pq só funciona com [0] mesmo que ainda retorne um vetor?
+
+        col_troca = np.where(Matriz[LinSaida, :] == 1)[0] #cb
+        
         teste = ProcurarColuna(col_troca, C)
         if(teste == 0):
-            trocar_colunas(Indice, col_troca[0])
-            trocar_colunas(Indice, col_troca[0], C)
             
-            return Matriz
+            cr[np.where(cr == Indice)[0][0]] = col_troca[0]
+            cb[np.where(cb == col_troca[0])[0][0]] = Indice
+            
+            R = ProcurarColuna(cr)
+            B = ProcurarColuna(cb)
+            return
     else:
-        print("Solução Ótima\n")
+        print("Matriz completa: \n", Matriz)
+        print("C completo: \n", C)
+        print("Cr novo: \n", cr)
+        print("Cb novo: \n", cb)
+        print("R novo: \n", R)
+        print("B novo: \n", B)
+        print("b novo: \n", b)
+        new_C = C.copy()
+        Solution = CalcFo()
+        print("Fo: \n", Solution)
+        sys.exit()
 
-def CalcCr():
-    cr = cr - cb * np.linalg.inv(B) * R
-    return cr
+#Calcular Cr =  cR − cB ⋅ B−1 ⋅ R✅
+def Calculo_Cr():
+    global cr, cb, C, B, R
+    cr_aux = ProcurarColuna(cr, C)
+    cb_aux = ProcurarColuna(cb, C)
+    Binverso = np.linalg.inv(B)
+    return cr_aux - cb_aux @ (Binverso @ R)
 
-def CalcR():
-    R = np.linalg.inv(B) * R
-    return R
+def Calculo_R():
+    global B, R, Matriz, cr
+    Binverso = np.linalg.inv(B)
+    R = Binverso @ R
 
 def CalcFo():
-    FO = cb * np.linalg.inv(B) * b
-    return FO
+    global cb, b, B
+    cb_aux = ProcurarColuna(cb, new_C)
+    print("b: ", b)
+    print("cb_aux: ", cb_aux)
+    Fo_val = cb_aux @ np.linalg.inv(B) @ b
+    return Fo_val
 
-EntrarBase()
-print("Matriz completa: \n", Matriz)
-print("C completo: \n", C)
-print("Cr novo: \n", cr)
-print("Cb novo: \n", cb)
-print("R novo: \n", R)
-print("B novo: \n", B)
-print("b novo: \n", b)
-
-#Testar se é ótima ✅
-#Calcular Cr =  cR − cB ⋅ B−1 ⋅ R✅
-#Trocar Colunas que entram e saem ✅
-#recalcular variáveis básicas b = B^-1 * b
-#recalcular R = B−1 ⋅ R
-#FO = cB ⋅ B−1 ⋅ b
+def Calcb():
+    global b, B
+    Binverso = np.linalg.inv(B)
+    b = Binverso @ b
+global new_C
+while True:
+    EntrarBase()
+    new_C = C.copy()
+    new_C[cr] = Calculo_Cr()
+    if(new_C[np.argmin(new_C[cr])] >= 0):
+        
+        Solution = CalcFo()
+        print("Fo: ", Solution)
+        sys.exit()
+    
+    Calcb()
+    Calculo_R()
+    trocar_colunas(Indice, col_troca[0])
+    trocar_colunas(Indice, col_troca[0], C)
+    Matriz[:, cr] = R
+    C[cr] = new_C[cr]
+    
+    #C[cb] = np.zeros(len(cb))
